af526d4b815ddc77ea260421faab119965818d3f
Lu Yibin <robinlu@me.com>
2016-09-29 11:23:36 +0800

    Initial Commit

---
+++ ImportForMe/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ImportForMe
+//
+//  Created by Lu Yibin on 29/09/2016.
+//  Copyright © 2016 Lu Yibin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ImportForMe/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ImportForMe
+//
+//  Created by Lu Yibin on 29/09/2016.
+//  Copyright © 2016 Lu Yibin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
